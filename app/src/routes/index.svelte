295fa859286d2249cdc0dc56eac368f7db1d399f
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-25 15:35:00 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -21,10 +21,10 @@
 					</div>
 					<div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
 						<h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-							<span class="block text-white">Все ремонтно-строительные </span>
+							<span class="block text-white">Все ремонтно-строительные</span>
 							<span class="block text-indigo-200">компании вашего города</span>
 						</h1>
-						<p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-4xl">В нашем каталоге представлены проверенные компании с хорошей репутацией, которые ведут деятельность по ремонту помещений, строительству зданий и сооружений, а также осуществляют продажу строительных и отделочных материалов.</p>
+						<p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-4xl">В нашем каталоге представлены проверенные компании с хорошей репутацией, которые ведут деятельность по ремонту помещений, строительству зданий и сооружений, а также осуществляют продажу строительных и отделочных материалов и их перевозку.</p>
 						<div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
 							<div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-1 sm:gap-5">
 								<!--								<a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"> Каталог компаний </a>-->
