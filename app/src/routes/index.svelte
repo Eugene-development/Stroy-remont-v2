ec08910a171fb356a17769ac45a72e9b42de21d1
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-25 15:13:53 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -15,13 +15,14 @@
 			<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 				<div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
 					<div class="absolute inset-0">
-						<img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100" alt="People working on laptops">
-						<div class="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply"></div>
+						<img class="h-full w-full object-cover" src="https://умс24.рф/wp-content/uploads/2020/01/1-9.jpg" alt="People working on laptops">
+						<div class="absolute inset-0 bg-slate-700 mix-blend-multiply"></div>
+<!--						<div class="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply"></div>-->
 					</div>
 					<div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
 						<h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-							<span class="block text-white">Все компании </span>
-							<span class="block text-indigo-200">по строительству и ремонту</span>
+							<span class="block text-white">Все ремонтно-строительные </span>
+							<span class="block text-indigo-200">компании вашего города</span>
 						</h1>
 						<p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-4xl">В нашем каталоге представлены проверенные компании с хорошей репутацией, которые ведут деятельность по ремонту помещений, строительству зданий и сооружений, а также осуществляют продажу строительных и отделочных материалов.</p>
 						<div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
