30dd4513370d0c9f091fb0542125eb67369015bb
indexpro24@gmail.com <indexpro24@gmail.com>
2022-05-30 15:03:37 +0300

    git init

--- app/src/components/header/section_01/v_001/index.svelte
+++ app/src/components/header/section_01/v_001/index.svelte
@@ -200,14 +200,14 @@
                 {/if}
             </nav>
             <div class="flex items-center md:ml-12">
-                <svg class="mr-4 h-7 w-7 text-slate-500" fill="none" stroke="currentColor"
+                <svg class="mr-4 h-7 w-7 text-slate-500 " fill="none" stroke="currentColor"
                      stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z"
                           stroke-linecap="round"
                           stroke-linejoin="round"/>
                 </svg>
 
-                <a href="tel:+74951475550" class="text-2xl font-medium text-slate-500 hover:text-slate-600"> 8 (495) 147-55-50 </a>
+                <a href="tel:+74951475550" class="text-2xl font-medium text-slate-500 hover:text-slate-600 font-semibold"> 8 (495) 147-55-50 </a>
             </div>
         </div>
     </div>
